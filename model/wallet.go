1781b2cf11bb646ce4cfb90d3c797a266026b987
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-16 14:21:04 +0700

    feat(model): add Transaction struct definition

--- model/wallet.go
+++ model/wallet.go
@@ -4,10 +4,10 @@ import "time"
 
 type Wallet struct {
 	ID int64
-	userID int64
-	walletNumber string
-	balance float64
-	status string
-	createdAt time.Time
-	updatedAt time.Time
+	UserID int64
+	WalletNumber string
+	Balance float64
+	Status string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
