a16e8eff32991a6e5a952344f4df162988300cb1
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-18 17:13:15 +0700

    feat: implement ShowTransferHistory function to display user's transaction history

---
+++ internal/menu/history_menu.go
@@ -0,0 +1,44 @@
+package menu
+
+import (
+	"fmt"
+
+	"koda-b8-ewallet-cli/internal/model"
+	"koda-b8-ewallet-cli/internal/service"
+)
+
+func ShowTransferHistory(user *model.User, transactionService *service.TransactionService) {
+
+	walletID, histories, err := transactionService.GetHistory(int(user.ID))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\n===== TRANSFER HISTORY =====")
+
+	if len(histories) == 0 {
+		fmt.Println("No transaction history.")
+		return
+	}
+
+	for i, h := range histories {
+
+		transactionType := "RECEIVED"
+		target := h.SenderWalletNumber
+
+		if h.SenderWalletID == walletID {
+			transactionType = "SENT"
+			target = h.ReceiverWalletNumber
+		}
+
+		fmt.Printf(
+			"%d. %-8s %-12s Rp %d (%s)\n",
+			i+1,
+			transactionType,
+			target,
+			h.Amount,
+			h.Status,
+		)
+	}
+}
